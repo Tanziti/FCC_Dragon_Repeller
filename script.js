89665f9ed8ea9eb714a254420da5324adf9c4a2b
Zi Tan <tanzitian3@gmail.com>
2024-03-29 15:10:37 -0400

    locations

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ const healthText = document.querySelector('#healthText');
 const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
-const monsterHealthText = document.querySelector('#monsterHealth')
+const monsterHealthText = document.querySelector('#monsterHealth');
+const locations = [{
+    name: 'town square'
+    }
+];
 
 // initialize buttons
 button1.onclick = goStore;
@@ -28,7 +32,7 @@ function goTown() {
     button1.onclick = goStore;
     button2.onclick = goCave;
     button3.onclick = fightDragon;
-    text.innerText = "You are in the town square. You see a sign that says Store.";
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
   }
 
 function goStore() {
@@ -53,4 +57,8 @@ function buyHealth(){
 }
 function buyWeapon(){
 
+}
+
+function update(location){
+  
 }
