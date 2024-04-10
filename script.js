51c26b835f058516ddac6ef28f9f3159d7216ee4
Zi Tan <tanzitian3@gmail.com>
2024-04-10 09:38:27 -0400

    finishing goFight

--- script.js
+++ script.js
@@ -149,6 +149,10 @@ function goCave() {
 
   function goFight(){
     update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
   }
   
   function fightSlime(){
