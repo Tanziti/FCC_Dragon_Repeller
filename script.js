3eceaff8f19a7940183215d91a9c256b3f1c5218
Zi Tan <tanzitian3@gmail.com>
2024-04-11 19:32:28 -0400

    character fight/attack

--- script.js
+++ script.js
@@ -173,6 +173,7 @@ function goCave() {
 function attack(){
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
 }
   
 function dodge(){
