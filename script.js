084597f05329c2b7701f3163460d725e5bd37e5e
Zi Tan <tanzitian3@gmail.com>
2024-06-17 21:23:17 -0400

    removing location

--- script.js
+++ script.js
@@ -69,18 +69,6 @@ const locations = [{
         text: 'You enter the cave. You see some monsters.'
     },
     {
-      name: "fight",
-      "button text": ["Attack","Dodge","Run"],
-      "button functions": [attack,dodge,goTown],
-      text: "You are fighting a monster."
-    },
-    {
-      name: "kill monster",
-      "button text": ["Go to town square","Go to town square","Go to town square"],
-      "button functions": [goTown,goTown,easterEgg],
-      text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
-    },
-    {
       name: "lose",
       "button text": ["REPLAY?","REPLAY?","REPLAY?"],
       "button functions": [restart,restart,restart],
@@ -91,13 +79,7 @@ const locations = [{
      "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
      "button functions": [restart, restart, restart],
      text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
-   },
-   {
-    name: 'easter egg',
-    "button text": ["2", "8", "Go to town square?"], 
-    "button functions": [pickTwo, pickEight, goTown], 
-    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!" 
-  }
+   }
 ];
 
 // initialize buttons
